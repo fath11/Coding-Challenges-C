119aa82379d368ba8363ce104b08bc54d1829ac3
Fath11 <fathimkamal@gmail.com>
2026-02-01 18:46:25 +0800

    improved code

--- src/ccwc/main.c
+++ src/ccwc/main.c
@@ -1,7 +1,10 @@
+#include <locale.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <wctype.h>
 
 int countByte(char buffer[], FILE *stream);
 int countLine(char buffer[], FILE *stream);
@@ -18,11 +21,10 @@ int countChar(char buffer[], FILE *stream);
  *              and argv[1] and every item after is the program's arguments.
  */
 int main(int argc, char *argv[]) {
-  char flag = argv[1][1]; // flag starts with "-"
-  char *filePath = argc == 2 ? argv[2] : argv[1];
+  char *filePath = argv[2] ? argv[2] : argv[1];
 
   FILE *stream;
-  if (filePath && filePath[1] != '-') {
+  if (filePath && filePath[0] != '-') {
     stream = fopen(filePath, "r");
     if (stream == NULL) {
       printf("Could not open file: %s", filePath);
@@ -30,73 +32,84 @@ int main(int argc, char *argv[]) {
     }
   } else {
     stream = stdin;
+    filePath = "";
   }
 
-  char buffer[1024] = {0};
+  int bytes = -1;
+  int lines = -1;
+  int words = -1;
+  int chars = -1;
 
-  int bytes = 0;
-  int lines = 0;
-  int words = 0;
-  int chars = 0;
-
-  int count;
-  bool found_whitespace = true;
-  bool is_in_multibyte = false;
-  while ((count = fread(buffer, 1, sizeof(*buffer), stream))) {
-    bytes += count;
-    for (int i = 0; i < count; i++) {
-      switch (buffer[i]) {
-      case '\n':
-        lines++;
-      case ' ':
-      case '\t':
-      case '\r':
-      case '\v':
-      case '\f':
-        found_whitespace = true;
-        break;
-      default:
-        if (found_whitespace == true) {
-          words++;
-        }
-        found_whitespace = false;
-        break;
-      }
-
-      // This stupid peice of shit is not accurate and I cannot for the life of
-      // me figure out why Please yell at me about how stupid my code is so I
-      // can fix it and sleep at night
-      if ((buffer[i] >> 7 == 0b0)) {
-        is_in_multibyte = false;
-        chars++;
-      } else {
-        if (is_in_multibyte == false) {
-          chars++;
-        }
-        is_in_multibyte = true;
-      }
+  int opt;
+  while ((opt = getopt(argc, argv, "clwm")) != -1) {
+    switch (opt) {
+    case 'c':
+      bytes = 0;
+      break;
+    case 'l':
+      lines = 0;
+      break;
+    case 'w':
+      words = 0;
+      break;
+    case 'm':
+      chars = 0;
+      break;
+    case '?':
+      printf("Invalid flag %c", opt);
+      exit(EXIT_FAILURE);
     }
   }
 
-  switch (flag) {
-  case 'c':
-    printf("%d ", bytes);
-    break;
-  case 'l':
-    printf("%d ", lines);
-    break;
-  case 'w':
-    printf("%d ", words);
-    break;
-  case 'm':
-    printf("%d ", chars);
-    break;
-  default:
-    printf("%d %d %d ", lines, words, bytes);
-    break;
+  if (bytes == -1 && lines == -1 && words == -1 && chars == -1) {
+    bytes = 0;
+    lines = 0;
+    words = 0;
   }
-  printf("%s\n", filePath ? filePath : "");
 
+  wint_t current_char;
+  char mbCharBuffer[4];
+  bool found_whitespace = true;
+  setlocale(LC_CTYPE, "");
+  while ((current_char = fgetwc(stream)) != WEOF) {
+    if (bytes >= 0) {
+      bytes += wctomb(mbCharBuffer, current_char);
+    }
+
+    if (lines >= 0 && current_char == '\n') {
+      lines++;
+    }
+
+    if (chars >= 0) {
+      chars++;
+    }
+
+    if (words >= 0) {
+      if (iswspace(current_char)) {
+        found_whitespace = true;
+        continue;
+      } else if (found_whitespace == true) {
+        words++;
+        found_whitespace = false;
+      }
+    }
+  }
   fclose(stream);
-  return EXIT_SUCCESS;
+
+  if (bytes >= 0) {
+    printf("%d ", bytes);
+  }
+  if (lines >= 0) {
+    printf("%d ", lines);
+  }
+  if (words >= 0) {
+    printf("%d ", words);
+  }
+  if (chars >= 0) {
+    printf("%d ", chars);
+  }
+
+  printf("%s\n", filePath);
+
+  exit(EXIT_SUCCESS);
 }
