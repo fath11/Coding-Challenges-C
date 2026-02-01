74dd4c570385026eba1c599c55423a776c91ef75
Fath11 <fathimkamal@gmail.com>
2026-02-01 15:32:54 +0800

    completed wc tool challenge

--- src/ccwc/main.c
+++ src/ccwc/main.c
@@ -19,70 +19,36 @@ int countChar(char buffer[], FILE *stream);
  */
 int main(int argc, char *argv[]) {
   char flag = argv[1][1]; // flag starts with "-"
-  char *filePath = argv[2];
+  char *filePath = argc == 2 ? argv[2] : argv[1];
 
-  FILE *fileStream = fopen(filePath, "r");
-  if (fileStream == NULL) {
-    printf("Could not open file: %s", filePath);
-    return EXIT_FAILURE;
-  }
-
-  char fileBuffer[1024] = {0};
-
-  switch (flag) {
-  case 'c':
-    printf("%d %s\n", countByte(fileBuffer, fileStream), filePath);
-    break;
-  case 'l':
-    printf("%d %s\n", countLine(fileBuffer, fileStream), filePath);
-    break;
-  case 'w':
-    printf("%d %s\n", countWord(fileBuffer, fileStream), filePath);
-    break;
-  case 'm':
-    printf("%d %s\n", countChar(fileBuffer, fileStream), filePath);
-    break;
-  default:
-    printf("Invalid flag: %c", flag);
-    fclose(fileStream);
-    return EXIT_FAILURE;
-  }
-
-  fclose(fileStream);
-  return EXIT_SUCCESS;
-}
-
-int countByte(char buffer[], FILE *stream) {
-  int output = 0;
-  int count = 0;
-  while ((count = fread(buffer, 1, sizeof(*buffer), stream))) {
-    output += count;
-  }
-  return output;
-}
-
-int countLine(char buffer[], FILE *stream) {
-  int output = 0;
-  int count = 0;
-  while ((count = fread(buffer, 1, sizeof(*buffer), stream))) {
-    for (int i = 0; i < count; i++) {
-      if (buffer[i] == '\n') {
-        output++;
-      }
+  FILE *stream;
+  if (filePath && filePath[1] != '-') {
+    stream = fopen(filePath, "r");
+    if (stream == NULL) {
+      printf("Could not open file: %s", filePath);
+      return EXIT_FAILURE;
     }
+  } else {
+    stream = stdin;
   }
-  return output;
-}
 
-int countWord(char buffer[], FILE *stream) {
-  int output = 0;
-  int count = 0;
+  char buffer[1024] = {0};
+
+  int bytes = 0;
+  int lines = 0;
+  int words = 0;
+  int chars = 0;
+
+  int count;
   bool found_whitespace = true;
+  bool is_in_multibyte = false;
   while ((count = fread(buffer, 1, sizeof(*buffer), stream))) {
+    bytes += count;
     for (int i = 0; i < count; i++) {
       switch (buffer[i]) {
-      case ' ':
       case '\n':
+        lines++;
+      case ' ':
       case '\t':
       case '\r':
       case '\v':
@@ -91,35 +57,46 @@ int countWord(char buffer[], FILE *stream) {
         break;
       default:
         if (found_whitespace == true) {
-          output++;
+          words++;
         }
         found_whitespace = false;
         break;
       }
-    }
-  }
 
-  return output;
-}
-
-// This stupid peice of shit is not accurate and I cannot for the life of me figure out why
-// Please yell at me about how stupid my code is so I can fix it and sleep at night
-int countChar(char buffer[], FILE *stream) {
-  int output = 0;
-  int count = 0;
-  bool is_in_multibyte = false;
-  while ((count = fread(buffer, 1, sizeof(*buffer), stream))) {
-    for (int i = 0; i < count; i++) {
+      // This stupid peice of shit is not accurate and I cannot for the life of
+      // me figure out why Please yell at me about how stupid my code is so I
+      // can fix it and sleep at night
       if ((buffer[i] >> 7 == 0b0)) {
         is_in_multibyte = false;
-        output++;
+        chars++;
       } else {
         if (is_in_multibyte == false) {
-          output++;
+          chars++;
         }
         is_in_multibyte = true;
       }
     }
   }
-  return output;
+
+  switch (flag) {
+  case 'c':
+    printf("%d ", bytes);
+    break;
+  case 'l':
+    printf("%d ", lines);
+    break;
+  case 'w':
+    printf("%d ", words);
+    break;
+  case 'm':
+    printf("%d ", chars);
+    break;
+  default:
+    printf("%d %d %d ", lines, words, bytes);
+    break;
+  }
+  printf("%s\n", filePath ? filePath : "");
+
+  fclose(stream);
+  return EXIT_SUCCESS;
 }
