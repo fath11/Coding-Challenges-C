48f9e982d2e8c1ca36a8bbefa8aacd62b05d9ce5
Fath11 <fathimkamal@gmail.com>
2026-02-02 09:58:53 +0800

    little tweaks for readability

--- src/ccwc/main.c
+++ src/ccwc/main.c
@@ -6,11 +6,6 @@
 #include <wchar.h>
 #include <wctype.h>
 
-int countByte(char buffer[], FILE *stream);
-int countLine(char buffer[], FILE *stream);
-int countWord(char buffer[], FILE *stream);
-int countChar(char buffer[], FILE *stream);
-
 /*
  * https://www.ibm.com/docs/en/i/7.6.0?topic=functions-main-function
  *
@@ -21,23 +16,9 @@ int countChar(char buffer[], FILE *stream);
  *              and argv[1] and every item after is the program's arguments.
  */
 int main(int argc, char *argv[]) {
-  char *filePath = argv[2] ? argv[2] : argv[1];
-
-  FILE *stream;
-  if (filePath && filePath[0] != '-') {
-    stream = fopen(filePath, "r");
-    if (stream == NULL) {
-      printf("Could not open file: %s", filePath);
-      return EXIT_FAILURE;
-    }
-  } else {
-    stream = stdin;
-    filePath = "";
-  }
-
-  int bytes = -1;
-  int lines = -1;
-  int words = -1;
+  int bytes = -1; // -1 means disabled.
+  int lines = -1; // Reusing variables here is fine because this is a really small project.
+  int words = -1; // I won't do this in bigger projects for my own sanity :)
   int chars = -1;
 
   int opt;
@@ -67,45 +48,61 @@ int main(int argc, char *argv[]) {
     words = 0;
   }
 
+  char *filePath = argv[2] ? argv[2] : argv[1];
+  FILE *stream;
+  if (filePath && filePath[0] != '-') {
+    stream = fopen(filePath, "r");
+    if (stream == NULL) {
+      printf("Could not open file: %s", filePath);
+      return EXIT_FAILURE;
+    }
+  } else {
+    stream = stdin;
+    filePath = "";
+  }
+
   wint_t current_char;
   char mbCharBuffer[4];
-  bool found_whitespace = true;
+  bool found_whitespace = true; // We assume the beginning of the file has a whitespace.
   setlocale(LC_CTYPE, "");
   while ((current_char = fgetwc(stream)) != WEOF) {
-    if (bytes >= 0) {
+    if (bytes != -1) {
+      // Convert the current widechracter to multibyte character and puts it in mbCharBuffer.
+      // This function also returns the size of the resulting multibyte character in bytes.
       bytes += wctomb(mbCharBuffer, current_char);
     }
 
-    if (lines >= 0 && current_char == '\n') {
+    if (lines != -1 && current_char == '\n') {
       lines++;
     }
 
-    if (chars >= 0) {
+    if (chars != -1) {
       chars++;
     }
 
-    if (words >= 0) {
+    if (words != -1) {
       if (iswspace(current_char)) {
         found_whitespace = true;
-        continue;
-      } else if (found_whitespace == true) {
+      } else if (found_whitespace == true) { 
+        // This code only runs when we found a non-whitespace character after a whitespace character.
         words++;
         found_whitespace = false;
       }
     }
   }
+
   fclose(stream);
 
-  if (bytes >= 0) {
+  if (bytes != -1) {
     printf("%d ", bytes);
   }
-  if (lines >= 0) {
+  if (lines != -1) {
     printf("%d ", lines);
   }
-  if (words >= 0) {
+  if (words != -1) {
     printf("%d ", words);
   }
-  if (chars >= 0) {
+  if (chars != -1) {
     printf("%d ", chars);
   }
 
